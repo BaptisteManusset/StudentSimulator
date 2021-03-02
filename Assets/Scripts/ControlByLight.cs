7035fedcec764c2d193770644997f3eb1b57e973
BaptisteManusset <Baptiste.Manusset@gmail.com>
2021-03-02 11:57:12 +0100

    sleep

---
+++ Assets/Scripts/ControlByLight.cs
@@ -0,0 +1,28 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class ControlByLight : MonoBehaviour
+{
+  //[SerializeField] [MinMaxSlider(0, 1)] Vector2 limit;
+  Light2D light;
+  [SerializeField] Light2D master;
+  public float breakpoint;
+  public bool forceOff = false;
+
+  void Start()
+  {
+    light = GetComponent<Light2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //light.enabled = (TimeManager.NormalizedDayAdvancement < limit.x || TimeManager.NormalizedDayAdvancement > limit.y) && !forceOff;
+    light.enabled = (master.intensity <= breakpoint) && !forceOff;
+  }
+
+
+  public void Open() => forceOff = false;
+  public void Close() => forceOff = true;
+}
