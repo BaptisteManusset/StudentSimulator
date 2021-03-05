b47eb5e3d7e3da0030f4ce5188a806be3373c7d5
BaptisteManusset <Baptiste.Manusset@gmail.com>
2021-03-05 15:45:14 +0100

    putain de pink tile

---
+++ Assets/OverlayController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayController : MonoBehaviour
+{
+
+  [SerializeField] GameObject overlay;
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.CompareTag("Player"))
+    {
+      overlay.SetActive(false);
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    if (collision.CompareTag("Player"))
+    {
+      overlay.SetActive(true);
+    }
+  }
+}
