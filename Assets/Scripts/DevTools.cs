6be9ee505da9bf672fed185949ecc0437ea16b0d
BaptisteManusset <Baptiste.Manusset@gmail.com>
2021-03-05 16:31:59 +0100

    pink fixed bordel de merde

---
+++ Assets/Scripts/DevTools.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DevTools : MonoBehaviour
+{
+
+  public Tilemap tileMap;
+
+  public List<TileBase> liste;
+  public List<TileBase> listeError;
+
+  [ContextMenu("TilemapList")]
+  void TilemapList()
+  {
+    liste.Clear();
+    for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++)
+    {
+      for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++)
+      {
+
+        Vector3Int localPlace = (new Vector3Int(n, p, (int)tileMap.transform.position.y));
+        Vector3 place = tileMap.CellToWorld(localPlace);
+
+
+        TileBase slt = tileMap.GetTile(localPlace);
+
+
+
+        if (slt)
+        {
+          Debug.Log($"Tiles:{slt} localPlace:{localPlace} place:{place}");
+          liste.Add(slt);
+        }
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/Editor/EditorTools.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+public class EditorTools : EditorWindow
+{
+  [MenuItem("Tools/EditorTools")]
+
+  public static void ShowWindow()
+  {
+    EditorWindow.GetWindow(typeof(EditorTools));
+  }
+
+  void OnGUI()
+  {
+    GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+  }
+}
